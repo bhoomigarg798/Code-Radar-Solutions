fdd293bc7af5e862f43d93a8c89fe6b4cc793b13
bhoomigarg798 <gargbhoomi031@gmail.com>
2025-01-31 12:39:53 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -2,44 +2,44 @@
 int main() {
     int day;
     scanf("%d",&day);
-    if (day==1){
+    if (day=='1'){
         printf("31");
     }
-    else if (day==2) {
-        printf("28");
-    }
-    else if (day==3) {
-        printf("30");
-    }
-    else if (day==4) {
-        printf("31");
-    }
-    else if(day==5) {
-        printf("30");
-    }
-    else if(day==6) {
-        printf("31");
-    }
-    else if (day==7) {
-        prinf("30");
-    }
-    else if (day==8) {
-        printf("31");
-    }
-    else if (day==9) {
-        printf("30");
-    }
-    else if (day==10) {
-        printf("31");
-    }
-    else if (day==11){
-        printf("30");
-    }
-    else if (day==12){
-        printf("31");
-    }
-    else {
-        printf("Invalid month");
-    }
-    return 0;
-}
+//     else if (day==2) {
+//         printf("28");
+//     }
+//     else if (day==3) {
+//         printf("30");
+//     }
+//     else if (day==4) {
+//         printf("31");
+//     }
+//     else if(day==5) {
+//         printf("30");
+//     }
+//     else if(day==6) {
+//         printf("31");
+//     }
+//     else if (day==7) {
+//         prinf("30");
+//     }
+//     else if (day==8) {
+//         printf("31");
+//     }
+//     else if (day==9) {
+//         printf("30");
+//     }
+//     else if (day==10) {
+//         printf("31");
+//     }
+//     else if (day==11){
+//         printf("30");
+//     }
+//     else if (day==12){
+//         printf("31");
+//     }
+//     else {
+//         printf("Invalid month");
+//     }
+//     return 0;
+// }
