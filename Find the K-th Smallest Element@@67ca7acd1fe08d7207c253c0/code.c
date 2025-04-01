3e75e0e33348ed78cb4503d6d94e84dd4cbc008f
Bhoomi Garg <gargbhoomi031@gmail.com>
2025-04-01 07:50:02 +0530

    Solution to Find the K-th Smallest Element

---
+++ Find the K-th Smallest Element@@67ca7acd1fe08d7207c253c0/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++) {
+        scanf("%d",&arr[i]);
+    }
+    int K;
+    scanf("%d",&K);
+    for (int i=0;i<n-1;i++) {
+        for (j=0;j<n-1-i;j++) {
+            if (arr[j]>arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    void printarray(int arr[],int n){
+        for (int i=0;i<n;i++) {
+            printf("%d",arr[n-K]);
+        }
+
+    }
+    return 0;
+}
